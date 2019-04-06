d113c784755081b231952fe59d8681ac00af2bf8
sathishkumar <amsathishkumar@users.noreply.github.com>
2019-04-06 13:23:41 +0530

    setup
    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+ 
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='pymod',
+      version='1.1',
+      description='A sat math package',
+      author='sat',
+      author_email='sat@tcl.org',
+      url='http://www.tcl.org/',
+      packages=['pymod'],
+     )
